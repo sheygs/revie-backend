4aa2a39031a5c81b702c611ee2236321bdc62202
sheygs13 <segun.ekoh@gmail.com>
2020-07-19 17:52:15 +0100

    feat(auth): login user

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const { JsonWebTokenError } = require('jsonwebtoken');
 const { log } = console;
 
 router.post('/register', async (req, res, next) => {
@@ -27,6 +28,29 @@ router.post('/register', async (req, res, next) => {
     await user.save();
     const token = user.generateAuthToken();
     res.status(201).json({ status: 'success', token });
+  } catch ({ message }) {
+    log(message);
+  }
+});
+
+router.post('/login', async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ status: 'error', error: 'All fields are required' });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ status: 'error', error: 'Invalid Email' });
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword)
+      return res
+        .status(400)
+        .json({ status: 'error', error: 'Invalid Password' });
+    const token = user.generateAuthToken();
+    res.status(200).json({ status: 'success', token });
   } catch ({ message }) {
     log(message);
   }
