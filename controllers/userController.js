d5e001f55e59399e2973f19ae21a54ca72c9206e
sheygs13 <segun.ekoh@gmail.com>
2020-07-19 18:35:26 +0100

    chore: refactor callback to controller

---
+++ controllers/userController.js
@@ -0,0 +1,58 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
+
+const registerUser = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res
+      .status(400)
+      .json({ status: 'error', error: 'All fields are required' });
+  }
+  try {
+    let user = await User.findOne({ email });
+    if (user)
+      return res
+        .status(400)
+        .json({ status: 'error', error: 'User already exists' });
+    const salt = await bcrypt.genSalt(10);
+    const hashed = await bcrypt.hash(password, salt);
+    user = new User({
+      name,
+      email,
+      password: hashed,
+    });
+    await user.save();
+    const token = user.generateAuthToken();
+    res.status(201).json({ status: 'success', token });
+  } catch ({ message }) {
+    next(message);
+  }
+};
+
+const loginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ status: 'error', error: 'All fields are required' });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({ status: 'error', error: 'Invalid Email' });
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword)
+      return res
+        .status(400)
+        .json({ status: 'error', error: 'Invalid Password' });
+    const token = user.generateAuthToken();
+    res.status(200).json({ status: 'success', token });
+  } catch ({ message }) {
+    next(message);
+  }
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,67 +1,17 @@
-const User = require('../models/user');
-const verifyToken = require('../middleware/auth');
+const authController = require('../middleware/auth');
+const userController = require('../controllers/userController');
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
 const { log } = console;
 
-router.post('/register', async (req, res, next) => {
-  const { name, email, password, phone } = req.body;
-  if (!name || !email || !password) {
-    return res
-      .status(400)
-      .json({ status: 'error', error: 'All fields are required' });
-  }
-  try {
-    let user = await User.findOne({ email });
-    if (user)
-      return res
-        .status(400)
-        .json({ status: 'error', error: 'User already exists' });
-    const salt = await bcrypt.genSalt(10);
-    const hashed = await bcrypt.hash(password, salt);
-    user = new User({
-      name,
-      email,
-      password: hashed,
-    });
-    await user.save();
-    const token = user.generateAuthToken();
-    res.status(201).json({ status: 'success', token });
-  } catch ({ message }) {
-    log(message);
-  }
-});
-
-router.post('/login', async (req, res, next) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res
-      .status(400)
-      .json({ status: 'error', error: 'All fields are required' });
-  }
-  try {
-    const user = await User.findOne({ email });
-    if (!user)
-      return res.status(400).json({ status: 'error', error: 'Invalid Email' });
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (!validPassword)
-      return res
-        .status(400)
-        .json({ status: 'error', error: 'Invalid Password' });
-    const token = user.generateAuthToken();
-    res.status(200).json({ status: 'success', token });
-  } catch ({ message }) {
-    log(message);
-  }
-});
-
-router.get('/verify', verifyToken, async (req, res, next) => {
+router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
+router.get('/verify', authController.verifyToken, async (req, res, next) => {
   try {
     log(req.user._id);
     res.send('Verified');
   } catch (error) {
-    log(error);
+    next(error);
   }
 });
 
